fe7c1f1b3cc42a27ccbbf1913c76ae65e02a179a
Sntosz <guguimaraes13@gmail.com>
2026-03-10 20:04:21 -0400

    style: login card

--- NailManager/src/App.tsx
+++ NailManager/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { CardDemo } from './components/ui/login_card'
+import { LoginCard } from './components/ui/login_card'
 
 function App() {
   return (
     <>
       <main className='min-h-screen bg-background flex items-center justify-center p-4'>
         <div className='w-screen h-screen flex items-center justify-center'>
-          <CardDemo></CardDemo>
+          <LoginCard></LoginCard>
 
         </div>
       </main>
--- NailManager/src/components/ui/login_card.tsx
+++ NailManager/src/components/ui/login_card.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,15 +6,17 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-export function CardDemo() {
+export function LoginCard() {
   return (
-    <Card className="w-full max-w-md min-h-120">
+    <Card className="w-full max-w-md min-h-100 flex">
       <CardHeader>
-        <CardTitle className="text-2xl font-bold text-card-foreground">Entre na sua conta</CardTitle>
+        <CardTitle className="text-2xl font-bold text-card-foreground">
+          Entre na sua conta
+        </CardTitle>
         <CardDescription className="text-muted-foreground mt-2">
           Insira suas credenciais para acessar sua conta.
         </CardDescription>
@@ -23,8 +25,13 @@ export function CardDemo() {
         <form>
           <div className="flex flex-col gap-6">
             <div className="grid gap-2">
-              <Label htmlFor="email" className="text-green-800">Email</Label>
-              <Input className="w-full h-10 pr-4 py-3 bg-input border border-border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent transition-all text-card-foreground placeholder:text-muted-foreground"
+              <Label htmlFor="email" className="text-green-800">
+                Email
+              </Label>
+              <Input
+                className="w-full h-10 pr-4 py-3 bg-input border border-border rounded-lg focus:outline-none focus:ring-2 
+                           focus:ring-primary focus:border-transparent transition-all text-card-foreground 
+                           placeholder:text-muted-foreground"
                 id="email"
                 type="email"
                 placeholder="m@example.com"
@@ -33,7 +40,9 @@ export function CardDemo() {
             </div>
             <div className="grid gap-2">
               <div className="flex items-center">
-                <Label htmlFor="password" className="text-green-800">Password</Label>
+                <Label htmlFor="password" className="text-green-800">
+                  Password
+                </Label>
                 <a
                   href="#"
                   className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
@@ -41,22 +50,33 @@ export function CardDemo() {
                   Esqueceu a senha?
                 </a>
               </div>
-              <Input className="w-full h-10 pr-4 py-3 bg-input border border-border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent transition-all text-card-foreground placeholder:text-muted-foreground" id="password" type="password" required />
+              <Input
+                className="w-full h-10 pr-4 py-3 bg-input border border-border rounded-lg focus:outline-none focus:ring-2 
+                           focus:ring-primary focus:border-transparent transition-all text-card-foreground 
+                           placeholder:text-muted-foreground"
+                id="password"
+                type="password"
+                required
+              />
             </div>
           </div>
         </form>
       </CardContent>
       <CardFooter className="flex-col gap-2">
-        <Button type="submit" className="w-full !bg-green-700 !text-white" variant="default">
-          Entrar
-        </Button>
-        <p className="text-sm text-muted-foreground">
-          Não tem uma conta?{" "}
-          <a href="#" className="underline-offset-4 hover:underline">
-            Cadastre-se
-          </a>
-        </p>
+          <Button
+            type="submit"
+            className="w-full bg-green-700! text-white!"
+            variant="default"
+          >
+            Entrar
+          </Button>
+          <p className="text-sm text-muted-foreground">
+            Não tem uma conta?{" "}
+            <a href="#" className="underline-offset-4 hover:underline">
+              Cadastre-se
+            </a>
+          </p>
       </CardFooter>
     </Card>
-  )
+  );
 }
